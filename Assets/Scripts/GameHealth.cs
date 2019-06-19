408761088b7a1a0c1b231ca8e10e3f9e52fa15bc
Greendead <rene97kg@googlemail.com>
2019-06-19 21:22:05 +0200

    Scenes erstellt, GameHealth erstellt
    MainMenu Szene,
    ManagerScene,
    TestLevel erstellt.
    GameHealth erstellt und Grundfunktionen eingefügt.

---
+++ Assets/Scripts/GameHealth.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHealth : MonoBehaviour
+{
+    public GameHealth Instance;
+    [SerializeField]
+    private TMPro.TMP_Text healthText;
+    private int healthLeft;
+
+
+
+
+    void Start()
+    {
+        if (Instance == null)
+            Instance = this;
+        healthLeft = 100;
+        UpdateHealthUI();
+    }
+
+
+
+    public void DeductHealth(int amount)
+    {
+        healthLeft -= amount;
+        UpdateHealthUI();
+    }
+
+
+
+    public void UpdateHealthUI()
+    {
+        healthText.text = healthLeft.ToString();
+    }
+}
